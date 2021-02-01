16f90a98de7467993590c1fcae7aae6325fdb360
Bruno <bcostinha01@gmail.com>
2021-02-01 16:27:19 +0000

    first commit: add project folders

---
+++ my-blog/src/pages/article-content.js
@@ -0,0 +1,23 @@
+const articles = [
+    {
+        name: 'learn-react',
+        title: 'The Fastest Way to Learn React',
+        content: [
+            `THIS IS THE FASTEST WAY TO LEARN REACT`,
+        ]
+    },    {
+        name: 'learn-node',
+        title: 'How to Build a Node Server in 10 Minutes',
+        content: [
+            `THIS IS HOW YOU BUILD A NODE SERVER IN 10 MINUTES`,
+        ]
+    },     {
+        name: 'my-thoughts-on-resumes',
+        title: 'My Thoughts on Resumes',
+        content: [
+            `THESE ARE MY THOUGHTS ON RESUMES`,
+        ]
+    },  
+];
+
+export default articles;
